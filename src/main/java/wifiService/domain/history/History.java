35e8301b64de06af37d8d1f4a274091923699881
하늘 <gureumi74@naver.com>
2025-01-12 12:46:09 +0900

    Refactor: Location -> History 테이블 이름 변경

--- src/main/java/wifiService/domain/history/History.java
+++ src/main/java/wifiService/domain/history/History.java
@@ -1,71 +1,13 @@
 package wifiService.domain.history;
 
-import wifiService.domain.wifi.Wifi;
+import lombok.Data;
 
 import java.sql.Timestamp;
 
+@Data
 public class History {
-    private Integer historyId;
-    private Integer locationId;
-    private Integer wifiId;
-    private Wifi wifi;
-    private String distance;
-    private Timestamp searchedAt;
-    private Integer bookmarkId; // 북마크 ID
-
-    public Integer getHistoryId() {
-        return historyId;
-    }
-
-    public void setHistoryId(Integer historyId) {
-        this.historyId = historyId;
-    }
-
-    public String getDistance() {
-        return distance;
-    }
-
-    public void setDistance(String distance) {
-        this.distance = distance;
-    }
-
-    public Integer getLocationId() {
-        return locationId;
-    }
-
-    public void setLocationId(Integer locationId) {
-        this.locationId = locationId;
-    }
-
-    public Integer getWifiId() {
-        return wifiId;
-    }
-
-    public void setWifiId(Integer wifiId) {
-        this.wifiId = wifiId;
-    }
-
-    public Wifi getWifi() {
-        return wifi;
-    }
-
-    public void setWifi(Wifi wifi) {
-        this.wifi = wifi;
-    }
-
-    public Timestamp getSearchedAt() {
-        return searchedAt;
-    }
-
-    public void setSearchedAt(Timestamp searchedAt) {
-        this.searchedAt = searchedAt;
-    }
-
-    public Integer getBookmarkId() {
-        return bookmarkId;
-    }
-
-    public void setBookmarkId(Integer bookmarkId) {
-        this.bookmarkId = bookmarkId;
-    }
+    private Integer id; // 히스토리 id
+    private double LAT; // 위도
+    private double LNT; // 경도
+    private Timestamp searchedAt; // 검색일시
 }
